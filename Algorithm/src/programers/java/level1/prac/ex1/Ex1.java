d2d57fe61c7056e67695c4e00d4459572ab9d362
namhyun99 <sshft@naver.com>
2022-04-08 21:17:31 +0900

    example

--- Algorithm/src/programers/java/level1/prac/ex1/Ex1.java
+++ Algorithm/src/programers/java/level1/prac/ex1/Ex1.java
@@ -15,7 +15,7 @@ public class Ex1 {
 	 */
 	
     public static void main(String[] args) {
-    	
+    	 
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
