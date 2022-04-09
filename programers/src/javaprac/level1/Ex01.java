897394d2efdc2daa0a579546843f6bed03c1b962
namhyun99 <sshft@naver.com>
2022-04-09 13:54:20 +0900

    copy example

---
+++ programers/src/javaprac/level1/Ex01.java
@@ -0,0 +1,32 @@
+package javaprac.level1;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	/*
+	 * 직사각형 별찍기
+	 * 
+	 * 입력 : 5 3
+	 * 출력결과 
+	 * *****
+	 * *****
+	 * *****
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a값 입력 : ");
+		int a = sc.nextInt();
+		System.out.print("b값 입력 : ");
+		int b = sc.nextInt();
+
+		for (int i = 0; i < b; i++) {
+			for (int j = 0; j < a; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
