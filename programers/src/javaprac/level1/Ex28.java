40c3874133dd5e16525844269cbdc6e104643307
namhyun99 <sshft@naver.com>
2022-05-20 15:33:36 +0900

    update

---
+++ programers/src/javaprac/level1/Ex28.java
@@ -0,0 +1,34 @@
+package javaprac.level1;
+
+import java.util.*;
+
+public class Ex28 {
+	// 같은 숫자는 싫어
+	public int[] solution(int[] arr) {
+		List<Integer> list = new ArrayList<>();
+		int value = -1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != value) {
+				list.add(arr[i]);
+				value = arr[i];
+			}
+		}
+		int[] answer = new int[list.size()];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = list.get(i);
+		}
+
+		return answer;
+	}
+
+	// 출력 테스트
+	public static void main(String[] args) {
+		Ex28 ex28 = new Ex28();
+
+		int[] arr1 = { 1, 1, 3, 3, 0, 1, 1 };
+		int[] arr2 = { 4, 4, 4, 3, 3 };
+		System.out.println(Arrays.toString(ex28.solution(arr1)));
+		System.out.println(Arrays.toString(ex28.solution(arr2)));
+	}
+
+}
---
+++ programers/src/javaprac/level1/Ex29.java
@@ -0,0 +1,27 @@
+package javaprac.level1;
+
+public class Ex29 {
+	// [1차] 다트게임
+	public int solution(String dartResult) {
+				
+		for(int i=0; i<dartResult.length(); i++) {	
+			if(dartResult.charAt(i) == 'S') {
+				
+			}
+			
+		}
+		
+
+		return 0;
+	}
+
+
+
+	public static void main(String[] args) {
+		Ex29 ex29 = new Ex29();
+
+		System.out.println(ex29.solution("1S2D*3T"));
+
+	}
+
+}
