f68503a9551bf98a024655e2898b0783cc105940
namhyun99 <sshft@naver.com>
2022-05-30 15:34:28 +0900

    update

--- programers/src/javaprac/level1/Ex20.java
+++ programers/src/javaprac/level1/Ex20.java
@@ -4,21 +4,35 @@ package javaprac.level1;
 public class Ex20 {
 
 	public int solution(int n) {
-		int answer = 0;
+		 int answer = 0;
+	        
+	        // 1. 에라토스테네스의 체로 거르기 위한 1차원 배열.
+	        boolean check[] = new boolean[n+1];
+	        
+	        // 2. 2부터 n까지의 수들 중
+	        for(int i =2;i<=n;i++){
+	            // 2-1. 소수의 배수로써 걸러진 수들은 넘어가고, (4,6,8,9..... 등)
+	            if(check[i] == true) continue;
+	            // 2-2. 자신을 제외한 배수를 고려하기 위해 i+i; j<=n;j=j+i 조건으로 걸러준다.
+	            for(int j=i + i;j<=n;j+=i){
+	                check[j] = true;
+	            }
+	        }
+	        
+	        // 3. 걸러지지 않은 수 들의 개수를 카운팅.
+	        for(int i=2;i<=n;i++){
+	            if(!check[i]) {
+	                answer++;
+	            }
+	        }
 
-		for (int i = 1; i < n; i++) {
-
-			if (n % i == 0) // 1,2,5,10
-				continue;
-
-		}
-
-		return answer;
+	        return answer;
 	}
 
 	public static void main(String[] args) {
 		Ex20 ex20 = new Ex20();
-		System.out.println(ex20.solution(10));
+//		System.out.println(ex20.solution(10));
+		System.out.println(ex20.solution(5));
 	}
 
 	// 1~10의 사이의 소수는 아래와 같다.
---
+++ programers/src/javaprac/level1/Ex30.java
@@ -0,0 +1,24 @@
+package javaprac.level1;
+
+public class Ex30 {
+	
+	public String solution(String s) {
+		String answer = "";
+		if(s.length() % 2 == 0) {
+			answer = s.substring(s.length()/2-1, s.length()/2+1);
+		}else {
+			answer = s.substring(s.length()/2, s.length()/2+1);
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		
+		Ex30 ex30 = new Ex30();
+		System.out.println(ex30.solution("abcde"));
+		System.out.println(ex30.solution("qwer"));
+		System.out.println(ex30.solution("abcdefg"));
+		
+	}
+
+}
