d8abf0ce72eda48f9c0969214fa3620b1b727d1e
namhyun99 <sshft@naver.com>
2022-08-16 23:22:13 +0900

    update

---
+++ programers/src/javaprac/level1/Ex35.java
@@ -0,0 +1,19 @@
+package javaprac.level1;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Ex35 {
+	
+	public String solution(int a, int b) {
+		String answer = "";
+		
+		Calendar cal= Calendar.getInstance();
+		System.out.println(cal);
+		
+		
+		
+		return answer;
+	}
+
+}
